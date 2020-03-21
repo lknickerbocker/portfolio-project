a7256753a04512f34871b05460a48feddd6386d0
Lance Knickerbocker <lhknickerbocker@gmail.com>
2020-03-20 22:32:34 -0400

    'Homepage'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=200) 
+    body = models.TextField(max_length=1000)
+    pub_date = models.DateTimeField()
+    
