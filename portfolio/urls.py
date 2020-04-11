113cad1b003a290788cd9690403ba29904264000
Lance Knickerbocker <lhknickerbocker@gmail.com>
2020-04-10 22:28:17 -0400

    another robot thing

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
+from jobs.views import robots_txt
 
 
 urlpatterns = [
