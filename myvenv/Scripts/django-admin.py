d727a818d263399f5198259ed812cef4bbc1417c
Lance Knickerbocker <lhknickerbocker@gmail.com>
2020-04-10 20:05:28 -0400

    fixed issue with venv and added pdf to static

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lhkni\dropbox\python\django-admin\portfolio-project\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
