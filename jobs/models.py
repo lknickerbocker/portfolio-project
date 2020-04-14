384f03feba877274be164d4d6e72c201f00fd29d
Lance Knickerbocker <lhknickerbocker@gmail.com>
2020-04-14 19:47:23 -0400

    placeholder model added

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,4 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
     linktoarticle = models.CharField(max_length=1000)
+    placeholder = models.IntegerField(default=1)
